8c4c0e5a1b83d49523ec246be7b51d3d7ff06c6c
Sage9705 <edem22kumahor@gmail.com>
2024-08-30 17:42:19 +0100

    Implement custom error handling middleware

---
+++ src/utils/errorHandler.js
@@ -0,0 +1,27 @@
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  // If Mongoose not found error, set to 404 and change message
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    statusCode = 400;
+    message = Object.values(err.errors)
+      .map((val) => val.message)
+      .join(", ");
+  }
+
+  res.status(statusCode).json({
+    message: message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
+};
+
+module.exports = errorHandler;
