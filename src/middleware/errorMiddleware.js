445912f9a86fed3dbf394ac840e6c72d9f29ea7b
Sage9705 <edem22kumahor@gmail.com>
2024-08-30 18:45:34 +0100

    Add custom error handling middleware

---
+++ src/middleware/errorMiddleware.js
@@ -0,0 +1,19 @@
+const logger = require("../utils/logger");
+
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  logger.error(`Error: ${err.message}`);
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
+};
+
+module.exports = { notFound, errorHandler };
